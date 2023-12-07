65fbbf392d4914899872271bf270d26d6479ee89
Evelyn Gualter <91782375+Evelyn5189@users.noreply.github.com>
2023-12-07 15:27:08 -0300

    Add files via upload

---
+++ modelo/Viagens.php
@@ -0,0 +1,166 @@
+<?php
+class Viagem{
+
+    private ?int $id;  
+    private string $tipo;
+    private string $nome;
+    private string $descricao;
+    private string $imagem;
+    private float $preco;
+
+    public function __construct(?int $id,
+                            string $tipo,
+                            string $nome,  
+                            string $descricao,  
+                            float $preco,
+                            string $imagem
+                            )
+{
+    $this->id = $id;
+    $this->tipo = $tipo;
+    $this->nome = $nome;
+    $this->descricao = $descricao;
+    $this->preco = $preco;
+    $this->imagem = $imagem;
+}
+
+ /**
+     * Get the value of id
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+
+
+    /**
+     * Get the value of tipo
+     */
+    public function getTipo(): string
+    {
+        return $this->tipo;
+    }
+
+    /**
+     * Set the value of tipo
+     */
+    public function setTipo(string $tipo): self
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+
+
+
+
+ /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+
+    /**
+     * Get the value of descricao
+     */
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setDescricao(string $descricao): self
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+
+
+
+ /**
+     * Get the value of preco
+     */
+    public function getPreco(): float
+    {
+        return $this->preco;
+    }
+
+    /**
+     * Set the value of preco
+     */
+    public function setPreco(float $preco): self
+    {
+        $this->preco = $preco;
+
+        return $this;
+    }
+
+
+
+
+     /**
+     * Get the value of imagem
+     */
+    public function getImagem(): string
+    {
+        return $this->imagem;
+    }
+    public function getImagemDiretorio(): string
+    {
+        return "../img/".$this->imagem;
+    }
+
+    /**
+     * Set the value of imagem
+     */
+    public function setImagem(string $imagem): void
+    {
+        $this->imagem = $imagem;
+
+       
+    }
+
+
+
+
+/*     function cadastrarviagem($codviagem, $nome, $datav, $preco, $duracao , $categoria, $img) {
+        $sql = "INSERT INTO viagens (codviagem, nome, datav, preco, duracao , categoria, img) VALUES 
+            (?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("sssssss", $codviagem, $nome, $datav, $preco, $duracao , $categoria, $img);
+        
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    } */
+}
